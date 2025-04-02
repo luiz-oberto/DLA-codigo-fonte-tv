aea4b62ac50df2c32cca7c3e26d60811f2759ad4
Luiz Oberto <lom.raiol@gmail.com>
2025-04-02 18:34:33 -0300

    Desafios dia 1 e 2

---
+++ Semana01/Dia02.py
@@ -0,0 +1,24 @@
+'''
+DLA - Desafios do dia 2
+
+# Desafio 01: Dados de um documento
+    - Classifique as informações do seu RG e CPF
+
+# Desafio 02: Identificando Tipos em um formulário da Caixa
+    - Classifique os tipos de dados do formulário
+
+# Desafio 03: Características de um iPhone14 Plus
+    - Identifique os campos utilizados para formar essa página
+'''
+
+'''
+Desafio 04: Calculo do IMC
+    - Calcule o sei IMC (Índice de Massa Corporal)
+    - Fórmula: IMC = peso/altura²
+'''
+
+peso: float = 80.0
+altura = 1.83
+
+imc = peso/altura**2
+print("O IMC é:", imc)
---
+++ Semana01/dia01.py
@@ -0,0 +1,17 @@
+"""
+# Desafio Lógica afiada - Dia 1
+
+Desafio 01 - Escreva seu algoritmo para acordar
+    - deve ser feito com papel e caneta
+    - não pode rasurar
+
+Desafio 02 - Escreva sua receita favorita
+    - mesmo esquema do primeiro desafio
+    - não pode ter menos de 20 itens
+
+Desafio 03 - Monte sua lista de tarefas diárias
+    - desde acordar até você dormir
+    - mais detalhado possível
+    - após criar a lista, tente imaginar como seria se cada um dos itens fosse um programa de computador que pudesse
+      ser elaborado de forma individual.
+"""
