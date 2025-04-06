7a7694bf2dd147ccdafbdba865eb877520c0c4f9
Luiz Oberto <lom.raiol@gmail.com>
2025-04-06 15:25:21 -0300

    Desafios dia 6 - completo.

--- Semana01/Dia06/DESAFIO-02.py
+++ Semana01/Dia06/DESAFIO-02.py
@@ -12,3 +12,31 @@ A ideia é que a lógica fique dentro de uma função.
 Faça várias chamadas da função para ver o
 resultado.
 """
+def obterDiaSemana(dia: int):
+    match dia:
+        case 1:
+            diaNome = "Domingo"
+        case 2:
+            diaNome = "Segunda-feira"
+        case 3:
+            diaNome = "terça-feira"
+        case 4:
+            diaNome = "Quarta-feira"
+        case 5:
+            diaNome = "Quinta-feira"
+        case 6:
+            diaNome = "Sexta-feira"
+        case 7:
+            diaNome = "Sábado"
+        case _:
+            ...
+    return print(diaNome)
+
+
+obterDiaSemana(1)
+obterDiaSemana(2)
+obterDiaSemana(3)
+obterDiaSemana(4)
+obterDiaSemana(5)
+obterDiaSemana(6)
+obterDiaSemana(7)
--- Semana01/Dia06/DESAFIO-03.py
+++ Semana01/Dia06/DESAFIO-03.py
@@ -11,3 +11,13 @@ financeira que rende um percentual ao ano. Calcule
 como esse investimento cresce ao longo do tempo
 ao ano.
 """
+def verRendimento(valor_incial: float, taxa_aa: float, anos: int, titulo: str):
+    print(f"Previsão do investimento: {titulo}")
+    for ano in range(1, anos+1):
+        valor_incial += taxa_aa * valor_incial
+        print(f'Ano {ano}, Total: R${valor_incial:.2f}')
+
+    return
+
+
+verRendimento(1000, 0.12, 10, 'Reserva de Emergência')
