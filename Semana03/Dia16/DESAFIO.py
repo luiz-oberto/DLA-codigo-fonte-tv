c46a5a9a89a6ba9b8c095001d40b72d9fc2b403a
Luiz Oberto <lom.raiol@gmail.com>
2025-05-06 16:17:15 -0300

    update Dia 16 - falta implementar change_order

--- Semana03/Dia16/DESAFIO.py
+++ Semana03/Dia16/DESAFIO.py
@@ -10,7 +10,7 @@ playlist deve ter as seguintes funcionalidades:
 # FEITO
 1. Capacidade de adicionar músicas
 
-# 
+# FEITO
 1.1 Capacidade de remover músicas
 
 # FEITO
@@ -27,7 +27,7 @@ qualquer momento
 # FEITO
 5. Função para tocar toda a playlist do início ao fim
 
-# 
+# FEITO
 6. Capacidade para tocar apenas uma música da
 playlist
 
@@ -36,7 +36,7 @@ playlist
 da música, nome do artista, Número de reproduções
 e Tempo total da música
 
-# 
+# FEITO
 8. Cada vez que uma música é tocada é preciso
 incrementar o número de reproduções
 
@@ -47,14 +47,21 @@ funcionalidades.
 
 Dica: Que tal utilizar um JSON para definir os
 atributos das músicas.
+
+# log de eventos do desenvolvimento
+- pensei em criar um registro de músicas criadas ao longo da execução do programa, 
+mas percebi que eu cresceria sem necessidade a complexidade do código, me fazendo 
+desistir da idéia e deixa por isso mesmo.
+
 '''
 class Music:
     # 7. ATRIBUTOS DA MÚSICA
-    def __init__(self, name: str, artist: str, reproductionTime):
+    def __init__(self, name: str, artist: str, reproductionTime: str):
         self.name = name
         self.artist = artist
         self.reproductions = 0
         self.reproductionTime = reproductionTime
+
 
     def play(self):
         self.reproductions+=1
@@ -75,10 +82,52 @@ class Playlist:
         print(f'{music.name} - {music.artist} adicionado a sua playlist')
 
         return
+    
+    
+    def delete_music(self):
+        print('\n# deseja deletar qual música?\n')
+        
+        for music in enumerate(self.musics):
+            print(f'{music[0]}. {music[1].name} - {music[1].artist}')
+        
+        index = input('$ Insira o índice-> ')
+        
+        try:
+
+            select_to_int = int(index)
+            select = input(f'excluir a música {self.musics[select_to_int].name}? [Y/n] ')
+            
+            match select:
+                case '':
+                    musica_deletada = self.musics.pop(select_to_int)
+                    print(f'A música {musica_deletada.name} foi excluída da playlist')
+                
+                case 'Y':
+                    musica_deletada = self.musics.pop(select_to_int)
+                    print(f'A música {musica_deletada.name} foi excluída da playlist')
+                
+                case 'y':
+                    musica_deletada = self.musics.pop(select_to_int)
+                    print(f'A música {musica_deletada.name} foi excluída da playlist')
+                
+                case 'n':
+                    print(f'Exclusão cancelada.')
+                
+                case 'N':
+                    print(f'Exclusão cancelada.')
+                
+                case _:
+                    print('Valor inválido \nExclusão cancelada.')
+
+        except:
+
+            return print('\níndice inserido inválido!')
+        
+        return
      
 
     # Mostrar todas as músicas da playlist
-    def show_playlist_musics(self, playlistName):
+    def show_playlist_musics(self, playlistName: str):
 
         if self.name == playlistName:
 
@@ -92,10 +141,24 @@ class Playlist:
 
     # Tocar playlist do inícion ao fim
     def play_playlist(self):
+        print(f'\n# Tocando playlist {self.name} #')
         for music in self.musics:
             music.play()
         return
     
+
+    # tocar uma música
+    def play_one_music(self, index: int):
+        try:
+            print(f"\nTocando {self.musics[index].name} de {self.musics[index].artist}")
+        except:
+            print('Índice inserido inválido')
+        return
+
+    def change_order(self):
+        return
+    
+###########################################################################################
 
 album = [
     {
@@ -132,17 +195,13 @@ album = [
 
 
 # criar música
-# musica1 = Music('Given Up', 'Linkin Park', '3:11')
+musica = Music('Dia Lindo', 'O Rappa', '6:10')
 
 # tocar música
 # musica1.play()
 
 # criar playlist
 lista_1 = Playlist('lista_1')
-
-# lista_2 = Playlist('lista_2')
-# lista_3 = Playlist('lista_3')
-# lista_1.insert_music(musica1)
 
 # adicionando músicas a playlist de acordo com a lista passada acima
 for i in album:
@@ -154,3 +213,11 @@ lista_1.show_playlist_musics('lista_1')
 
 # tocar playlist
 lista_1.play_playlist()
+
+# deletar música
+# lista_1.delete_music()
+
+# tocar apenas uma música
+lista_1.play_one_music(0)
+
+# lista_1.show_playlist_musics('lista_1')
